db7dd6d2ac16d7e64fd8dff6cd4ea7319f89c9fc
Alexander Levin <alexanderjlevin1@gmail.com>
2018-01-21 17:32:07 -0500

    not quite working, but on section 5

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express(); // creates instance of express application
 const volleyball = require('volleyball');
 app.use(volleyball);
+const nunjucks = require('nunjucks');
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+// // nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+
+// const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// // res.render( 'index', {title: 'Hall of Fame', people: people} );
+
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
 
 app.use(function (req, res, next) {
     console.log('Request Type:', req.method);
