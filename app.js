bede5ffa992f053d668151421a2e344aed33b428
Alexander Levin <alexanderjlevin1@gmail.com>
2018-01-21 17:46:46 -0500

    on section 7

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const nunjucks = require('nunjucks');
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true});
-// // nunjucks.render('index.html', locals, function (err, output) {
-//     console.log(output);
-// });
+ nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+ });
 
 // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 // // res.render( 'index', {title: 'Hall of Fame', people: people} );
-
 
 var locals = {
     title: 'An Example',
@@ -22,7 +21,6 @@ var locals = {
         { name: 'Hermione'}
     ]
 };
-
 
 app.use(function (req, res, next) {
     console.log('Request Type:', req.method);
